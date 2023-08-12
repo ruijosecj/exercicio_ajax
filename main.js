b31881b0764ec5eaf4ae4e6872f34608d0b02dba
ruijosecj <ruijosecj@gmail.com>
2023-08-12 15:02:11 -0300

    exercicio ajax

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const nameElement = document.querySelector('#name');
+    const userNameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+   
+    fetch('https://api.github.com/users/ruijosecj')
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(json){
+        nameElement.innerHTML = json.name;
+        userNameElement.innerHTML = json.login;
+        avatarElement.src = json.avatar_url;
+        followingElement.innerHTML = json.following;
+        followersElement.innerHTML = json.followers;
+        reposElement.innerHTML = json.public_repos;
+        linkElement.href = json.html_url;
+    })
+})
